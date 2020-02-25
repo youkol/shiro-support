60df8bbbf6a66bb62caf159a7c7e354ec0fb3ba1
jackiea <jackiea_21@163.com>
2020-02-25 13:47:10 +0800

    Change package name.

---
+++ src/main/java/com/youkol/support/shiro/service/JwtUserAccount.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.youkol.support.shiro.service;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author jackiea
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@SuperBuilder
+public class JwtUserAccount extends DefaultUserAccount {
+
+    private static final long serialVersionUID = 1L;
+
+    private String token;
+
+    private boolean tokenCorrect;
+    
+}
--- src/main/java/com/youkol/support/shiro/web/filter/CaptchaFormAuthenticationFilter.java
+++ src/main/java/com/youkol/support/shiro/web/filter/CaptchaFormAuthenticationFilter.java
@@ -1,20 +1,34 @@
-package com.youkol.shiro.web.filter;
+/**
+ * Copyright (C) 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.youkol.support.shiro.web.filter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.youkol.shiro.authc.CaptchaUsernamePasswordToken;
-import com.youkol.shiro.authc.IncorrectCaptchaException;
-import com.youkol.shiro.web.util.WebTools;
+import com.youkol.support.shiro.authc.CaptchaUsernamePasswordToken;
+import com.youkol.support.shiro.authc.IncorrectCaptchaException;
+import com.youkol.support.shiro.web.util.WebTools;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
